c709ec782e9ed40e9c8e5d5b5a082be0c249fbe9
TeemuV <temppi166@gmail.com>
2024-11-20 21:38:06 +0200

    #2: Included navbar in project and commented unneccessary buttons

---
+++ tmas/app/layout.jsx
@@ -0,0 +1,22 @@
+import '@/assets/styles/globals.css'
+
+export const metadata ={
+  title: 'CardManiac',
+  description:'All your cardboard needs',
+  keywords:'card, mythic'
+
+
+}
+
+const MainLayout = () => {
+  return (
+    <html lang='en'>
+      <body>
+        <div>MainLayout</div>
+      </body>
+    </html>
+  )
+    
+}
+// Comment test
+export default MainLayout
